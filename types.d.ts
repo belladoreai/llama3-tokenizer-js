67ac10944ac92659b6a4afcb61c120f962d61a4c
Belladore <135602125+belladoreai@users.noreply.github.com>
2024-04-22 02:18:58 +0300

    Fix types.

--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,13 @@
-export declare class LlamaTokenizer {
+export declare class Llama3Tokenizer {
     vocabById: string[];
     vocabByString: Map<string, number>;
     merges: Map<string, number>;
     constructor(vocab_base64?: string, merges_binary?: string);
     encode(prompt: string, add_bos_token?: boolean, add_preceding_space?: boolean, log_performance?: boolean): number[];
     decode(tokenIds: number[], add_bos_token?: boolean, add_preceding_space?: boolean): string;
+    getSpecialTokenId(tokenString: string): number;
     runTests(tests?: (tokenizer: LlamaTokenizer) => boolean): void
 }
-declare const llamaTokenizer: LlamaTokenizer;
-export default llamaTokenizer;
+declare const llama3Tokenizer: Llama3Tokenizer;
+export default llama3Tokenizer;
 //# sourceMappingURL=types.d.ts.map
