daa9fa5d818a5fa45dbcc670f3c5636171273b50
Belladore <135602125+belladoreai@users.noreply.github.com>
2024-05-03 16:32:50 +0300

    Fix types.

--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,17 @@
+type EncodeOptions = {
+    bos?: boolean,
+    eos?: boolean
+}
+
 export declare class Llama3Tokenizer {
     vocabById: string[];
     vocabByString: Map<string, number>;
     merges: Map<string, number>;
     constructor(vocab_base64?: string, merges_binary?: string);
-    encode(prompt: string, add_bos_token?: boolean, add_preceding_space?: boolean, log_performance?: boolean): number[];
-    decode(tokenIds: number[], add_bos_token?: boolean, add_preceding_space?: boolean): string;
+    encode(prompt: string, options?: EncodeOptions): number[];
+    decode(tokenIds: number[]): string;
     getSpecialTokenId(tokenString: string): number;
-    runTests(tests?: (tokenizer: LlamaTokenizer) => boolean): void
+    runTests(tests?: (tokenizer: Llama3Tokenizer) => boolean): void
 }
 declare const llama3Tokenizer: Llama3Tokenizer;
 export default llama3Tokenizer;
